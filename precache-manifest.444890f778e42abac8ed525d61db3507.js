2b93c7733b9cdbee114245891e24a62b79a67f6b
Abir <abbasa@uoguelph.ca>
2019-01-20 22:28:37 -0500

    Updates

---
+++ precache-manifest.444890f778e42abac8ed525d61db3507.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Shopify-Waste-Wizard/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "6b81d3f2e29f107311bc",
+    "url": "/Shopify-Waste-Wizard/static/css/main.0e18b364.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Shopify-Waste-Wizard/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "dd9fdedacded33b1f8b3",
+    "url": "/Shopify-Waste-Wizard/static/js/1.dd9fdeda.chunk.js"
+  },
+  {
+    "revision": "4b8edd4e8dea5d694b7f",
+    "url": "/Shopify-Waste-Wizard/static/js/runtime~main.4b8edd4e.js"
+  },
+  {
+    "revision": "6b81d3f2e29f107311bc",
+    "url": "/Shopify-Waste-Wizard/static/js/main.6b81d3f2.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Shopify-Waste-Wizard/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Shopify-Waste-Wizard/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Shopify-Waste-Wizard/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "dd9fdedacded33b1f8b3",
+    "url": "/Shopify-Waste-Wizard/static/css/1.512f775e.chunk.css"
+  },
+  {
+    "revision": "9a57c229cbf56fc9245b6791d9c946d5",
+    "url": "/Shopify-Waste-Wizard/index.html"
+  }
+];
